28b963ed67e92d76ca172f17e7a17c12cb79055f
박지민 <pjm5489@naver.com>
2026-01-17 23:04:30 +0900

    feat: iOS 환경별 BASE_URL 설정 기능 추가
    * [Feat]: AppConfig에서 Info.plist의 BASE_URL을 참조하도록 수정
    * [Build]: Info.plist에 BASE_URL 키 추가 및 xcconfig 파일 연결
    * [Chore]: .gitignore에 xcconfig 파일 추가

--- composeApp/src/iosMain/kotlin/com/daedan/festabook/config/AppConfig.kt
+++ composeApp/src/iosMain/kotlin/com/daedan/festabook/config/AppConfig.kt
@@ -3,9 +3,12 @@ package com.daedan.festabook.config
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.Inject
+import platform.Foundation.NSBundle
 
 @Inject
 @ContributesBinding(AppScope::class)
 actual class AppConfig {
-    actual val baseUrl: String = ""
+    actual val baseUrl: String =
+        NSBundle.mainBundle.objectForInfoDictionaryKey("BASE_URL") as? String
+            ?: error("Config 파일에 BASE_URL 없음")
 }
