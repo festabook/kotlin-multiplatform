b6bb41d4ac811a95c790fe9e2dcd2dec5de66f67
박지민 <pjm5489@naver.com>
2026-01-18 23:28:32 +0900

    refactor: ApiResult의 Network 라이브러리를 Retrofit에서 Ktorfit으로 변경

--- composeApp/src/commonMain/kotlin/com/daedan/festabook/data/datasource/remote/ApiResult.kt
+++ composeApp/src/commonMain/kotlin/com/daedan/festabook/data/datasource/remote/ApiResult.kt
@@ -1,9 +1,9 @@
 package com.daedan.festabook.data.datasource.remote
 
+import de.jensklingenberg.ktorfit.Response
+import io.ktor.client.statement.request
 import kotlinx.coroutines.CancellationException
-import okio.IOException
-import retrofit2.Response
-import timber.log.Timber
+import kotlinx.io.IOException
 
 sealed class ApiResult<out T> {
     data class Success<T>(
@@ -43,41 +43,44 @@ sealed class ApiResult<out T> {
                         val body = response.body()
                         when {
                             body != null -> {
-                                Timber.d("RES 200 $requestMethod : $requestUrl \n $body")
+//                                Timber.d("RES 200 $requestMethod : $requestUrl \n $body")
                                 Success(body)
                             }
 
-                            response.code() == 204 -> {
-                                Timber.d("RES 204 $requestMethod : $requestUrl")
+                            response.code == 204 -> {
+//                                Timber.d("RES 204 $requestMethod : $requestUrl")
                                 @Suppress("UNCHECKED_CAST")
                                 Success(Unit as T)
                             }
 
                             else -> {
-                                Timber.e("ERR ${response.code()} $requestMethod $requestUrl - Unknown success case")
+//                                Timber.e("ERR ${response.code()} $requestMethod $requestUrl - Unknown success case")
                                 UnknownError
                             }
                         }
                     } else {
-                        val errorBody = response.errorBody()?.string()
-                        Timber.e("ERR ${response.code()} $requestMethod $requestUrl \n $errorBody")
-                        when (response.code()) {
+                        val errorBody = response.errorBody()?.toString()
+//                        Timber.e("ERR ${response.code()} $requestMethod $requestUrl \n $errorBody")
+                        when (response.code) {
                             in 400..499 -> {
                                 ClientError(
-                                    response.code(),
-                                    response.message(),
-                                    response.errorBody()?.string(),
+                                    response.code,
+                                    response.message,
+                                    errorBody,
                                 )
                             }
 
-                            in 500..599 ->
+                            in 500..599 -> {
                                 ServerError(
-                                    response.code(),
-                                    response.message(),
-                                    response.errorBody()?.string(),
+                                    response.code,
+                                    response.message,
+                                    errorBody,
                                 )
+                            }
 
-                            else -> UnknownError
+                            else -> {
+                                UnknownError
+                            }
                         }
                     }
                 }.getOrElse { error ->
