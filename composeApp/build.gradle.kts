6a8f7bb966b4fe8f5788df4c48498734697651cc
박지민 <pjm5489@naver.com>
2026-01-22 23:32:21 +0900

    build: DataStore 의존성 추가
    * build: gradle/libs.versions.toml에 DataStore 버전 및 라이브러리 정의 추가
    * build: composeApp 모듈에 DataStore 라이브러리 의존성 추가

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -66,6 +66,8 @@ kotlin {
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.kotlinx.datetime)
+            implementation(libs.androidx.datastore)
+            implementation(libs.androidx.datastore.preferences)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
