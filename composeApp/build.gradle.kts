2f3bd69fa4471634f054b803ee4366f629c20174
박지민 <pjm5489@naver.com>
2026-01-23 21:58:04 +0900

    [Build]: Metro Runtime 라이브러리 관리 방식 변경
    * build: `libs.versions.toml`에 Metro Runtime 의존성 추가
    * build: `composeApp/build.gradle.kts`에서 하드코딩된 Metro Runtime 버전을 Version Catalog로 대체

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,7 +41,7 @@ kotlin {
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
-            export("dev.zacsweers.metro:runtime:0.9.4")
+            export(libs.metro.runtime)
         }
     }
 
