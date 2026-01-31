593ec2d6ce6b62cc7a357d475b72702d8443e270
박지민 <pjm5489@naver.com>
2026-01-31 22:43:31 +0900

    feat: DataStore 마이그레이션 로직 추가

--- composeApp/src/commonMain/kotlin/com/daedan/festabook/data/db/DatabaseFileName.kt
+++ composeApp/src/commonMain/kotlin/com/daedan/festabook/data/db/DatabaseFileName.kt
@@ -1,3 +1,7 @@
 package com.daedan.festabook.data.db
 
-internal const val DATASTORE_FILE_NAME = "festabook_prefs.preferences"
+// 기존에 있는 SharedPreferences 파일명
+internal const val OLD_PREFS_FILE_NAME = "app_prefs"
+
+// 새로운 DataStore의 파일명
+internal const val DATASTORE_FILE_NAME = "festabook_prefs"
