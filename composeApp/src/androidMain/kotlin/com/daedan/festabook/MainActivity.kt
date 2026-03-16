759081033069133e3cb125be02f9b79d310544d9
oungsi2000 <oungsi1000@gmail.com>
2026-03-16 19:15:54 +0900

    feat: 플랫폼별 진입점 Scaffold 적용 및 이미지 로더 설정
    * **feat: 앱 진입점(`MainActivity`, `MainViewController`) 레이아웃 개선**
        * `Scaffold`를 추가하여 시스템 인셋(innerPadding)이 `PlaceMapRoute`에 적절히 반영되도록 수정했습니다.
        * `Modifier.padding`을 통해 UI가 상태 표시줄이나 내비게이션 바에 가려지지 않도록 최적화했습니다.
    
    * **feat: Coil 이미지 로더 의존성 주입 설정**
        * `CompositionLocalProvider`를 통해 `LocalCoilImageLoader`를 제공하도록 수정했습니다.
        * **Android**: `imageLoader` 인스턴스를 연동했습니다.
        * **iOS**: `SingletonImageLoader`를 통해 플랫폼 컨텍스트에 맞는 이미지 로더를 연동했습니다.
    
    * **refactor: iOS 플랫폼 네이버 지도 카메라 애니메이션 수정**
        * `CameraAnimation.Easing` 상태의 매핑 값을 `NMFCameraUpdateAnimationEaseOut`에서 `NMFCameraUpdateAnimationEaseIn`으로 변경했습니다.
    
    * **chore: iOS ViewModel 팩토리 설정 동기화**
        * `MainViewController`에서 `viewModel` 생성 시 `metroVmf`를 명시적으로 사용하도록 수정하여 Android와 일관성을 맞췄습니다.

--- composeApp/src/androidMain/kotlin/com/daedan/festabook/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/daedan/festabook/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil3.imageLoader
 import com.daedan.festabook.presentation.placeMap.component.PlaceMapRoute
 import com.daedan.festabook.presentation.platform.rememberLocationSource
 import com.daedan.festabook.presentation.theme.FestabookTheme
+import com.skydoves.landscapist.coil3.LocalCoilImageLoader
 import dev.zacsweers.metrox.viewmodel.LocalMetroViewModelFactory
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -24,13 +29,19 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             FestabookTheme {
-                CompositionLocalProvider(LocalMetroViewModelFactory provides metroVmf) {
-                    PlaceMapRoute(
-                        placeMapViewModel = viewModel(factory = metroVmf),
-                        onStartPlaceDetail = {},
-                        onShowErrorSnackBar = {},
-                        locationSource = rememberLocationSource(),
-                    )
+                CompositionLocalProvider(
+                    LocalMetroViewModelFactory provides metroVmf,
+                    LocalCoilImageLoader provides imageLoader,
+                ) {
+                    Scaffold { innerPadding ->
+                        PlaceMapRoute(
+                            placeMapViewModel = viewModel(factory = metroVmf),
+                            onStartPlaceDetail = {},
+                            onShowErrorSnackBar = {},
+                            locationSource = rememberLocationSource(),
+                            modifier = Modifier.padding(innerPadding),
+                        )
+                    }
                 }
             }
         }
--- composeApp/src/iosMain/kotlin/com/daedan/festabook/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/daedan/festabook/MainViewController.kt
@@ -1,14 +1,21 @@
 package com.daedan.festabook
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil3.SingletonImageLoader
+import coil3.compose.LocalPlatformContext
 import com.daedan.festabook.di.IosAppGraph
 import com.daedan.festabook.presentation.placeMap.component.PlaceMapRoute
 import com.daedan.festabook.presentation.platform.rememberLocationSource
 import com.daedan.festabook.presentation.theme.FestabookTheme
+import com.skydoves.landscapist.coil3.LocalCoilImageLoader
 import dev.zacsweers.metro.createGraph
 import dev.zacsweers.metrox.viewmodel.LocalMetroViewModelFactory
+import platform.Foundation.NSLog
 
 private val festabookAppGraph = createGraph<IosAppGraph>()
 private val metroVmf = festabookAppGraph.metroViewModelFactory
@@ -17,13 +24,19 @@ private val metroVmf = festabookAppGraph.metroViewModelFactory
 fun MainViewController() =
     ComposeUIViewController {
         FestabookTheme {
-            CompositionLocalProvider(LocalMetroViewModelFactory provides metroVmf) {
-                PlaceMapRoute(
-                    placeMapViewModel = viewModel(),
-                    onStartPlaceDetail = {},
-                    onShowErrorSnackBar = {},
-                    locationSource = rememberLocationSource(),
-                )
+            CompositionLocalProvider(
+                LocalMetroViewModelFactory provides metroVmf,
+                LocalCoilImageLoader provides SingletonImageLoader.get(LocalPlatformContext.current),
+            ) {
+                Scaffold { innerPadding ->
+                    PlaceMapRoute(
+                        placeMapViewModel = viewModel(factory = metroVmf),
+                        onStartPlaceDetail = {},
+                        onShowErrorSnackBar = {},
+                        locationSource = rememberLocationSource(),
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
             }
         }
     }
