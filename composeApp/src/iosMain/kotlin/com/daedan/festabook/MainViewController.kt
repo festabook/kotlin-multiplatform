e7eb48afa18ae2118717d0404b16af2aa1fb5bb8
oungsi2000 <oungsi1000@gmail.com>
2026-03-29 17:36:37 +0900

    refactor: iOS `MainViewController` 내 미사용 임포트 제거
    * **refactor: 불필요한 POSIX exit 임포트 삭제**
        * `MainViewController.kt`에서 사용되지 않는 `platform.posix.exit` 임포트 구문을 제거하여 코드를 정리했습니다.

--- composeApp/src/iosMain/kotlin/com/daedan/festabook/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/daedan/festabook/MainViewController.kt
@@ -3,7 +3,6 @@ package com.daedan.festabook
 import androidx.compose.ui.window.ComposeUIViewController
 import com.daedan.festabook.presentation.FestabookScreen
 import com.daedan.festabook.presentation.theme.FestabookTheme
-import platform.posix.exit
 
 @Suppress("ktlint:standard:function-naming")
 fun MainViewController() =
