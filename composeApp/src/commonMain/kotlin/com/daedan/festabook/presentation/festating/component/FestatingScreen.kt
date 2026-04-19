01038455a6572ba35636616c3d15a5927e16ae30
박지민 <pjm5489@naver.com>
2026-04-19 21:05:51 +0900

    [Feat] FestatingScreen 내 웹뷰 뒤로가기 기능 구현
    * **feat: 웹뷰 내비게이션 백 핸들링 추가**
        * `NavigationBackHandler`를 사용하여 웹뷰 내에서 뒤로 이동이 가능한 경우(`navigator.canGoBack`), 시스템 뒤로가기 동작 시 웹뷰의 이전 페이지로 이동하도록 로직을 구현했습니다.
        * 뒤로가기 이벤트 상태 관리를 위해 `rememberNavigationEventState`를 추가했습니다.
    
    * **refactor: 웹뷰 상태 변수명 변경**
        * 기존 `state`로 정의되었던 웹뷰 상태 변수를 `webViewState`로 변경하여 용도를 명확히 했습니다.

--- composeApp/src/commonMain/kotlin/com/daedan/festabook/presentation/festating/component/FestatingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/daedan/festabook/presentation/festating/component/FestatingScreen.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigationevent.NavigationEventInfo
+import androidx.navigationevent.compose.NavigationBackHandler
+import androidx.navigationevent.compose.rememberNavigationEventState
 import com.daedan.festabook.BuildKonfig
 import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewNavigator
@@ -17,16 +20,21 @@ fun FestatingScreen(
     festivalId: Long,
     modifier: Modifier = Modifier,
 ) {
-    val state =
+    val webViewState =
         rememberWebViewState(
             url = BuildKonfig.FESTA_TING_URL,
             additionalHttpHeaders = mapOf(HEADER_FESTIVAL_ID to festivalId.toString()),
         )
+    val backState = rememberNavigationEventState(NavigationEventInfo.None)
     val navigator = rememberWebViewNavigator()
+
+    NavigationBackHandler(state = backState, isBackEnabled = navigator.canGoBack) {
+        navigator.navigateBack()
+    }
 
     Scaffold(modifier = modifier) { innerPadding ->
         WebView(
-            state = state,
+            state = webViewState,
             navigator = navigator,
             modifier = Modifier.fillMaxSize().padding(innerPadding),
         )
