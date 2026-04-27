ca807eed8cd1295dc9371231882965448e61fd35
oungsi2000 <oungsi1000@gmail.com>
2026-04-28 02:32:15 +0900

    style: 홈 화면 및 테스트 코드 내 불필요한 임포트 최적화
    * **style: `HomeScreen`, `HomeViewModel`, `HomeViewModelTest` 임포트 순서 정리**
        * 패키지 가독성 향상을 위해 관련 도메인 레포지토리 및 UI 상태 모델의 임포트 순서를 일관되게 조정했습니다.
        * 중복되거나 잘못 정렬된 임포트 구문을 정리했습니다.

--- composeApp/src/commonMain/kotlin/com/daedan/festabook/presentation/home/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/daedan/festabook/presentation/home/HomeViewModel.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.daedan.festabook.di.viewmodel.ViewModelKey
 import com.daedan.festabook.domain.repository.FestivalRepository
-import com.daedan.festabook.presentation.home.model.toUiModel
 import com.daedan.festabook.domain.repository.MyWaitingRepository
+import com.daedan.festabook.presentation.home.model.toUiModel
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesIntoMap
 import dev.zacsweers.metro.Inject
